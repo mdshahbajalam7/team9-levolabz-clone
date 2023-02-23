a86337ab02c7bf0da3665b722794eb85c1b3f9cc
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2023-02-23 19:34:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
       <Developers />
       <Bestdevelopers />
       <PeopleTuring />
-      {/* <InformationSlider /> */}
+      <InformationSlider />
       <RemoteDevelopre />
       <Footer />
     </>
